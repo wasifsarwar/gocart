74a53f7e5a2268ac6066b9f032d1458113b73101
Wasif Siddique <wasif.sarwar9@gmail.com>
2025-04-23 12:29:54 -0700

    update user and product models

--- product-service/internal/models/product.go
+++ product-service/internal/models/product.go
@@ -1,7 +1,7 @@
 package models
 
 type Product struct {
-	ID          string  `json:"id"`
+	ProductID   string  `json:"product_id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float64 `json:"price"`
--- user-service/models/user.go
+++ user-service/models/user.go
@@ -1,8 +1,13 @@
 package models
 
+import "time"
+
 type User struct {
-	UserID    string `json:"user_id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
+	UserID    string    `json:"user_id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Phone     string    `json:"phone"`
 }
